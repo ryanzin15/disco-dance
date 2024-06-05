035cb875813c90a52fe60c8b25968b9d5cf267d3
ryanzin15 <165925742+ryanzin15@users.noreply.github.com>
2024-06-05 11:52:16 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.showLeds(`
+    # # # # #
+    # # . # #
+    # . # . #
+    # # . # #
+    # # # # #
+    `)
+basic.forever(function () {
+    for (let index = 0; index < input.soundLevel(); index++) {
+    	
+    }
+})
+basic.forever(function () {
+    led.setBrightness(255)
+})
